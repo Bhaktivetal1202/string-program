12394d30130360fa60245fa20a2c5a1fcbd50bde
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-08-31 19:23:02 +0530

    Add files via upload

---
+++ accept string and display concatnation.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[100],s2[100];
+	printf("enter first string");
+	gets(s1);
+	printf("enter second string");
+	gets(s2);
+	strcat(s1,s2);
+	printf("\n first string=%s",s1);
+	printf("\n second string=%s",s2);
+}

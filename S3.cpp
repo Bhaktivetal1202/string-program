1bf211a81cb8dc10532b2e4cf5a96b98a9612f9b
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-09-27 18:15:53 +0530

    Add files via upload

---
+++ S3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char name[]={'r','b','n','b','\0'};
+	char addr[]="shrirampur";
+	printf("\n name=%s",name);
+	printf("\n address=%s",addr);	
+}

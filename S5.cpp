821b0102aeaaca34b6afb9f836fc37dc86eb2967
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-09-30 18:17:46 +0530

    Add files via upload

---
+++ S5.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[20];
+	int i,n=0;
+	printf("enter string");
+	gets(s1);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		n++;
+	}
+	printf("length of string=%d",n);
+}

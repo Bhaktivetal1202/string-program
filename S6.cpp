a63ad4787677f156b3969e8bc06c2ab7bdc442de
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-01 17:54:13 +0530

    Add files via upload

---
+++ S6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[50],s2[50];
+	int i;
+	printf("enter string");
+	gets(s1);
+	for(i=0;s1[i]!='\0';i++);
+	printf("\n reverse string=");
+	for(i-1;i>=0;i--)
+	printf("%c",s1[i]);
+}

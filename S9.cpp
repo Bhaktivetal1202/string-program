fcdf574a155df756d12391b85870a5f7888bb1b8
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-04 18:05:06 +0530

    Add files via upload

---
+++ S9.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[100],s2[50];
+	int i,c1=0,c2=0;
+	printf("enter first string");
+	gets(s1);
+	printf("enter second string");
+	gets(s2);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		c1++;
+	}
+	for(i=0;s2[i]!='\0';i++)
+	{
+		c2++;
+	}
+	if(c1<c2)
+	printf("first string is smaller=%s",s1);
+	else
+	printf("second string is smaller=%s",s2);
+}

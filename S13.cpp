38cf5ba29e5ea37a586316af2aedf1ea77debf90
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-08 17:53:16 +0530

    Add files via upload

---
+++ S13.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+	char s1[100];
+	int i,c1=0,c2=0,c3=0,c4=0;
+	printf("enter the string=");
+	gets(s1);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		if(isupper(s1[i]))
+		c1++;
+		else if(islower(s1[i]))
+		c2++;
+		else if(isdigit(s1[i]))
+		c3++;
+		else 
+		c4++;
+	}
+	printf("\n upeercase character=%d",c1);
+	printf("\n lowercase character=%d",c2);
+	printf("\n digit=%d",c3);
+	printf("\n symbol=%d",c4);
+}
